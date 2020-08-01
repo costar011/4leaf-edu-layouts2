1805fb0b93f1d86aeddf9f5e95b50bc69b7bbbe7
YERIM <dnaer456@gmail.com>
2020-08-01 14:05:56 +0900

    css

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ app.use(express.static(path.join(__dirname, "/assets")));
 
 // "/" 를 get해서 사용자에게 보여주는 웹페이지
 app.get("/", (req, res) => {
-  res.render("");
+  res.render("index");
 });
 
 app.listen(PORT, () => {
